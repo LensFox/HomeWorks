040784419ffd6977a18bfdab44d442f1be28e1bd
o.ivankova <missheleninwonderland@gmail.com>
2022-08-02 13:06:30 +0200

    1-Lesson1 Homework (not finished)

---
+++ 1-Collections/MonthsCollection.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Collections
+{
+    internal class MonthsCollection<Month> : ICollection<Month>
+    {
+        Month[] months = new Month[0];
+        public int Count { get { return months.Length; } set { } }
+        public bool IsReadOnly { get { return false; } }
+        public void Add(Month month)
+        {
+            Month[] newMonths = new Month[months.Length + 1];
+            months.CopyTo(newMonths, 0);
+            newMonths[newMonths.Length - 1] = month;
+            months = newMonths;
+        }
+        public void Clear()
+        {
+            months = new Month[0];
+        }
+        public bool Contains(Month month)
+        {
+            foreach (Month item in months)
+            {
+                if (item.Equals(month))
+                    return true;
+            }
+            return false;
+        }
+        public void CopyTo(Month[] arr, int index)
+        {
+            months.CopyTo(arr, index);
+        }
+        public bool Remove(Month month)
+        {
+            if (months.Contains(month))
+            {
+                var newMonths = new Month[months.Length - 1];
+
+                int j = 0;
+                for (int i = 0; i < months.Length; i++)
+                {
+                    if (months[i].Equals(month))
+                    {
+                        continue;
+                    }
+
+                    newMonths[j] = months[i];
+                    j++;
+                }
+                months = newMonths;
+                return true;
+            }
+            return false;
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (this as IEnumerable<Month>).GetEnumerator();
+        }
+        public IEnumerator<Month> GetEnumerator()
+        {
+            return ((IEnumerable<Month>)months).GetEnumerator();
+        }
+    }
+}
---
+++ 1-Collections/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1_Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Month jan = new Month("January", 31, 1);
+            Month may = new Month("May", 31, 5);
+            Month sep = new Month("September", 30, 9);
+
+            MonthsCollection<Month> year = new MonthsCollection<Month>();
+            year.Add(jan);
+            year.Add(may);
+            year.Add(sep);
+
+            foreach (Month month in year)
+                Console.WriteLine($"{month.Name}, {month.DaysInMonth}, {month.OrderNumber}");
+
+            Console.WriteLine($"Contains May? {year.Contains(may)}");
+
+            year.Remove(may);
+
+            Console.WriteLine($"Contains May? {year.Contains(may)}");
+            
+            foreach (Month month in year)
+                Console.WriteLine($"{month.Name}, {month.DaysInMonth}, {month.OrderNumber}");
+        }
+    }
+}
