0af91a7a9211a9fe7b8610dcce9bf6c731400bf0
o.ivankova <missheleninwonderland@gmail.com>
2022-08-16 15:15:15 +0200

    9-Homework

---
+++ 9-GarbageCollector/Monitoring.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _9_GarbageCollector
+{
+    internal static class Monitoring
+    {
+        internal static void Monitor(long memoryLimit)
+        {
+            var memoryUsed = GC.GetTotalMemory(false);
+            Console.WriteLine(memoryUsed);
+            try
+            {
+                if (memoryUsed > memoryLimit && memoryLimit != 0)
+                {
+                    throw new Exception("You have exceeded the memory limit");
+                }
+                else if (memoryLimit == 0)
+                {
+                    throw new Exception("Invalid input format");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ 9-GarbageCollector/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _9_GarbageCollector
+{
+    /*
+     Дополнительное задание
+Создайте свой класс, объекты которого будут занимать много места в памяти (например, в
+коде класса будет присутствовать большой массив) и реализуйте для этого класса,
+формализованный шаблон очистки.  
+    Задание 2
+Создайте класс, который позволит выполнять мониторинг ресурсов, используемых программой.
+Используйте его в целях наблюдения за работой программы, а именно: пользователь может
+указать приемлемые уровни потребления ресурсов (памяти), а методы класса позволят выдать
+предупреждение, когда количество реально используемых ресурсов приблизиться к
+максимально допустимому уровню. 
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long memoryLimit = 0;
+            try
+            {
+                Console.WriteLine("Hello! Enter the memory limit");
+                memoryLimit = Convert.ToInt64(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            using (BigArrayClass ins = new())
+            {
+                Console.WriteLine(ins.arr[2]);
+                Monitoring.Monitor(memoryLimit);
+            }
+        }
+    }
+}
