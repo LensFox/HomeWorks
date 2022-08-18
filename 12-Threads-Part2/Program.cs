200a7d8767bb099d2a83c2cafce0b54a0ab249f8
o.ivankova <missheleninwonderland@gmail.com>
2022-08-18 15:30:52 +0200

    12-Threads Homework

---
+++ 12-Threads-Part2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Threading;
+
+namespace _12_Threads_Part2
+{
+    /*
+     Дополнительное задание
+Создайте Semaphore, осуществляющий контроль доступа к ресурсу из нескольких потоков.
+Организуйте упорядоченный вывод информации о получении доступа в специальный *.log
+файл. 
+     */
+    internal class Program
+    {
+        static Semaphore _pool;
+        static readonly string _path = @"F:\Tests\Threads.log";
+        static void Main(string[] args)
+        {
+            _pool = new Semaphore(1, 5, "MySemaphore"); //работает только с одним потоком
+            for (int i = 0; i < 3; i++)
+            {
+                new Thread(Method).Start(i);
+            }
+        }
+        static void Method(object i)
+        {
+            _pool.WaitOne();
+            var writer = new StreamWriter(_path, true);
+            writer.WriteLine($"Stream {i} worked");
+            writer.Close();
+            _pool.Release();
+        }
+    }
+}
---
+++ 12-Treads-Part2-Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace _12_Treads_Part2_Task2
+{
+    /*
+     Задание 2
+Преобразуйте пример событийной блокировки таким образом, чтобы вместо ручной обработки
+использовалась автоматическая. 
+     */
+    internal class Program
+    {
+        static AutoResetEvent auto = new AutoResetEvent(false);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Thread thread = new Thread(Method);
+            thread.Start();
+            Console.ReadLine();
+            auto.Set();
+        }
+        static void Method()
+        {
+            Console.WriteLine("I do smth");
+            auto.WaitOne();
+            Console.WriteLine("I keep doing smth");
+        }
+    }
+}
