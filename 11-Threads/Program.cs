df0ad743e74e365166fecb4bf992f7aef377e4f2
o.ivankova <missheleninwonderland@gmail.com>
2022-08-23 12:15:29 +0200

    14,15 - Homework

--- 11-Threads/Program.cs
+++ 11-Threads/Program.cs
@@ -1,40 +1,45 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace _11_Threads
 {
     /*
      Дополнительное задание
-Используя конструкции блокировки, создайте метод, который будет в цикле for (допустим, на 10
+Переделайте дополнительное задание из урока №11 с использованием конструкции async
+await. 
+    
+(Используя конструкции блокировки, создайте метод, который будет в цикле for (допустим, на 10
 итераций) увеличивать счетчик на единицу и выводить на экран счетчик и текущий поток.
 Метод запускается в трех потоках. Каждый поток должен выполниться поочередно, т.е. в
 результате на экран должны выводиться числа (значения счетчика) с 1 до 30 по порядку, а не в
-произвольном порядке.
+произвольном порядке.)
      */
     internal class Program
     {
         static int counter;
-        static object block = new object();
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Task task1 = new(IncrementCounterAsync);
+            Task task2 = new Task(IncrementCounterAsync);
+            Task task3 = new Task(IncrementCounterAsync);
+            task1.Start();
+            await task1;
+            task2.Start();
+            await task2;
+            task3.Start();
+            await task3;
 
-            Thread thread1 = new Thread(new ThreadStart(IncrementCounter));
-            Thread thread2 = new Thread(new ThreadStart(IncrementCounter));
-            IncrementCounter();
-            thread1.Start();
-            thread2.Start();
         }
-        internal static void IncrementCounter()
+        internal async static void IncrementCounterAsync()
         {
-            lock (block)
+            for (int i = 0; i < 10; i++)
             {
-                for (int i = 0; i < 10; i++)
-                {
-                    counter++;
-                    Console.WriteLine("Thread: {0}, Counter: {1}", Thread.CurrentThread.ManagedThreadId, counter);
-                }
+                counter++;
+                Console.WriteLine("Thread: {0}, Counter: {1}", Thread.CurrentThread.ManagedThreadId, counter);
             }
+            await Task.Delay(0);
         }
     }
 }
---
+++ 14-TLP/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _14_TLP
+{
+    /*
+    Дополнительное задание
+Создайте массив чисел размерностью в 1 000 000 или более. Используя генератор случайных
+чисел, проинициализируйте этот массив значениями. Создайте PLINQ запрос, который
+позволит получить все нечетные числа из исходного массива. 
+    ShowOddNumbers()
+
+    Задание 2
+Создайте два метода, которые будут выполняться в рамках параллельных задач. Организуйте
+вызов этих методов при помощи Invoke таким образом, чтобы основной поток программы
+(метод Main) не остановил свое выполнение. 
+     */
+    internal class Program
+    {
+        static int[] array;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ShowOddNumbers();
+
+            Task<int> task1 = new(Task1);
+            Task task2 = new(Task2);
+            task1.Start();
+            task2.Start();
+            Task.WaitAll(task1, task2);
+            Console.WriteLine("Result of Task1: {0}", task1.Result);
+        }
+        static void ShowOddNumbers()
+        {
+            array = new int[1000000];
+            Random random = new Random();
+            Parallel.For(0, array.Length, i => array[i] = random.Next());
+
+            ParallelQuery<int> oddNumbers = from el in array.AsParallel()
+                                            where el % 2 == 0
+                                            select el;
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(oddNumbers.ToArray()[i]);
+            }
+        }
+        static int Task1()
+        {
+            return array[5] * array[6] / 2;
+        }
+        static void Task2()
+        {
+            array[5] = 1;
+            Console.WriteLine("Task 2 shows: {0}, {1}, {2}", array[4], array[5], array[6]);
+        }
+    }
+}
